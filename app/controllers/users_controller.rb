5306c6129e7b0237c8333bad2f98e742e57422db
Frank <frank@frank-Inspiron-N4030.(none)>
2013-03-14 23:39:05 +0800

    Modify user controller and adjust some link_to.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,27 +1,14 @@
 class UsersController < ApplicationController
 
   def index
-    @posts = Post.all
+    @users = User.all
   end
 
   def show
-    @user = current_user
+    @user = User.find(params[:id])
     respond_to do |format|
       format.html
       format.json { render json: @user }
-    end
-  end
-
-  def update
-    @user = current_user
-    respond_to do |format|
-      if @user.update_attributes(params[:user])
-        format.html { redirect_to @post, notice: 'User was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @user.errors, status: :unprocessable_entity }
-      end
     end
   end
 
