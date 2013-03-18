304fc92b343cf7c208869b28ba9d1788125b8773
Frank <frank@frank-Inspiron-N4030.(none)>
2013-03-18 23:52:37 +0800

    Upload image in posts.

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,7 +1,12 @@
 class Post < ActiveRecord::Base
 	has_many :comments, :dependent => :destroy
 	belongs_to :user
-  attr_accessible :content, :title, :user_id
+  attr_accessible :content, :title, :user_id, :avatar, :avatar_cache
 
   validates :content, :title, presence: true
+  mount_uploader :avatar, AvatarUploader
+
+  def has_avatar
+    self.avatar_url rescue nil
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130318114313) do
+ActiveRecord::Schema.define(:version => 20130318153524) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(:version => 20130318114313) do
     t.datetime "created_at", :null => false
     t.datetime "updated_at", :null => false
     t.integer  "user_id"
+    t.string   "avatar"
   end
 
   create_table "users", :force => true do |t|
