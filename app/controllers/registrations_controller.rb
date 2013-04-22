fbe221d7b6cba4d27642b169e0dd04488c5cc1b7
Frank <frank@frank-Inspiron-N4030.(none)>
2013-04-22 13:15:12 +0800

    Modify registrations_controller.

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -2,16 +2,8 @@ class RegistrationsController < Devise::RegistrationsController
 
   protected
 
-  def after_sign_up_path_for(resource)
-    if resource.avatar.blank?
-      root_path
-    else
-      crop_users_path
-    end
-  end
-
   def after_update_path_for(resource)
-    if resource.avatar.blank?
+    if params[:user][:avatar] == nil
       root_path
     else
       crop_users_path
